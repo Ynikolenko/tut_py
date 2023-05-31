d2831d31dda40ff7610f6263dc5eefb4ffd3c685
Yuriy Nikolenko <yurka@yandex.ru>
2023-05-31 11:44:35 +0200

    String Formatting

--- StringPropertiesAndMethods/properties_and_methods.py
+++ StringPropertiesAndMethods/properties_and_methods.py
@@ -19,8 +19,12 @@ print(greeting)
 
 # Multiplication
 yummy = 'Yum '
-print(yummy * 2)
+print(yummy * 2) # вывести два раза
 
-print(yummy.upper())
-print(yummy.lower())
+print(yummy.upper()) # вывести результат заглавными
+print(yummy.lower()) # вывести результат маленькими
 print(yummy)
+
+long_string = 'Это самая длиная строка в программе'
+print(long_string.split()) # разбивка строки по словам
+print(long_string.split('а')) # убирает из строки символ
